3c8be24224ac8813b7ec51caabff4fa179fc48e7
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-07-30 23:30:20 +0500

    add input search

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 
 import Head from 'next/head'
 
+import { Input } from 'antd'
+import axios from 'axios'
+
 import MapboxMap from '../../component/MapboxMap'
+import useDebounce from '../../util/hooks/useDebounce'
 
 import styles from './Map.module.scss'
 
 const Map = () => {
   const [loading, setLoading] = React.useState(true)
   const handleMapLoading = () => setLoading(false)
+  const [search, setSearch] = useState<string>('')
+  const debouncedSearch = useDebounce(search, 500)
 
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+  }
+
+  useEffect(() => {
+    axios.get(`${process.env.API_URL}/map/place/ru`, {
+      params: { name: debouncedSearch },
+    })
+  }, [debouncedSearch])
   return (
     <>
       <Head>
         <title>Using mapbox-gl with React and Next.js</title>
       </Head>
       <div className={styles.appContainer}>
+        <div className={styles.search}>
+          <div className={styles.searchContainer}>
+            <Input
+              size="large"
+              placeholder="Введите город"
+              onChange={handleSearch}
+            />
+          </div>
+        </div>
         <div className={styles.mapWrapper}>
           <MapboxMap
             initialOptions={{ center: [38.0983, 55.7038] }}
