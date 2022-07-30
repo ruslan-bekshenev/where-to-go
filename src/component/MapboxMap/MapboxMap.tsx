325afba73adca7c3568a588a6735111ae866225e
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-07-30 21:47:14 +0500

    init map

---
+++ src/component/MapboxMap/MapboxMap.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from 'react'
+
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+export interface MapboxMapProps {
+  initialOptions?: Omit<mapboxgl.MapboxOptions, 'container'>
+  onCreated?(map: mapboxgl.Map): void
+  onLoaded?(map: mapboxgl.Map): void
+  onRemoved?(): void
+  onMapLoaded?: any
+}
+
+const MapboxMap = ({
+  initialOptions = {},
+  onCreated,
+  onLoaded,
+  onRemoved,
+}: MapboxMapProps) => {
+  const [map, setMap] = useState<mapboxgl.Map>()
+
+  const mapNode = useRef(null)
+
+  useEffect(() => {
+    const node = mapNode.current
+
+    if (typeof window === 'undefined' || node === null) return
+
+    const mapboxMap = new mapboxgl.Map({
+      container: node,
+      accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40],
+      zoom: 9,
+      ...initialOptions,
+    })
+
+    setMap(mapboxMap)
+    if (onCreated) onCreated(mapboxMap)
+
+    if (onLoaded) mapboxMap.once('load', () => onLoaded(mapboxMap))
+
+    return () => {
+      mapboxMap.remove()
+      setMap(undefined)
+      if (onRemoved) onRemoved()
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return <div ref={mapNode} style={{ width: '100%', height: '100%' }} />
+}
+
+export default MapboxMap
--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
+import Head from 'next/head'
+
+import MapboxMap from '../../component/MapboxMap'
+
+import styles from './Map.module.scss'
+
 const Map = () => {
+  const [loading, setLoading] = React.useState(true)
+  const handleMapLoading = () => setLoading(false)
+
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis
-      culpa distinctio doloribus id incidunt non praesentium quae reiciendis
-      sed.
-    </div>
+    <>
+      <Head>
+        <title>Using mapbox-gl with React and Next.js</title>
+      </Head>
+      <div className={styles.appContainer}>
+        <div className={styles.mapWrapper}>
+          <MapboxMap
+            initialOptions={{ center: [38.0983, 55.7038] }}
+            onMapLoaded={handleMapLoading}
+          />
+        </div>
+        {/*{loading && <MapLoadingHolder className="loading-holder" />}*/}
+      </div>
+    </>
   )
 }
 
