2d89de11803d8315e46c6136ca8aa98d1c4494f4
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-08-06 03:27:42 +0500

    feature: add env interface, env example

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,12 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_MAPBOX_TOKEN: string
+      NEXT_PUBLIC_API_URL: string
+    }
+  }
+}
